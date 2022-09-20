b5ffa681788300d320d8586e32520d932568c1d3
radar <763567579@qq.com>
2022-09-20 17:32:53 +0800

    123

---
+++ src/com/bjpowernode/domain/Person.java
@@ -0,0 +1,12 @@
+package com.bjpowernode.domain;
+
+/**
+ * @version: java version 1.8
+ * @Author: My Radar
+ * @description:
+ * @date: 2022-09-20 17:29
+ */
+public class Person {
+    private String id;
+
+}
